a6505b87c8e19bd1b8917afcab72b135852f47e8
Dhruv Jain <dhruvjain0412@gmail.com>
2025-01-11 01:09:59 +0530

    Fixed Bugs

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-// Reload the page when Home link is clicked
 document.getElementById("home-link").onclick = function () {
   window.location.reload();
 };
 
-// Smooth scroll to Features section when Features link is clicked
 document
   .getElementById("features-link")
   .addEventListener("click", function (event) {
-    event.preventDefault();
+    event.preventDefault(); 
     document
       .getElementById("features-section")
       .scrollIntoView({ behavior: "smooth" });
   });
 
-// Smooth scroll to Contact section when Contact link is clicked
 document.getElementById("contact-link").addEventListener("click", function (e) {
-  e.preventDefault(); // Prevent default link behavior
+  e.preventDefault(); 
   document.getElementById("contact").scrollIntoView({
-    behavior: "smooth", // Smooth scrolling
-    block: "start", // Align to the top of the viewport
+    behavior: "smooth",
+    block: "start",
   });
 });
